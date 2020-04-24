867c2c60e98bc307b34ee6ba8cc3f003e13a572e
fegzycole <fergusoniyara@gmail.com>
2020-04-24 17:34:32 +0100

    Add searhicon to header and enable eslint locally

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Logo from '../assets/disc.png';
+import DiscLogo from '../assets/disc.png';
+import SearchIcon from '../assets/search.png';
 
 const Header = () => (
   <header>
     <div className="icon">
-      <img src={Logo} alt="music-disc" />
+      <img src={DiscLogo} alt="music-disc" />
+      <h3>Songify</h3>
     </div>
+
+    <img src={SearchIcon} alt="search-icon" />
   </header>
-)
+);
 
 export default Header;
