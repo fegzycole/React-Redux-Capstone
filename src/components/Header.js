cfad6cdb3b097bc89a75097cd37ed032cb6eefb9
fegzycole <fergusoniyara@gmail.com>
2020-04-24 17:16:42 +0100

    Add some logic to header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../assets/disc.png';
+
+const Header = () => (
+  <header>
+    <div className="icon">
+      <img src={Logo} alt="music-disc" />
+    </div>
+  </header>
+)
+
+export default Header;
