ac2ef9bdd517facbf2558e3c19238ff77ea0a03a
fegzycole <fergusoniyara@gmail.com>
2020-04-24 17:47:40 +0100

    Add search box to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DiscLogo from '../assets/disc.png';
 import SearchIcon from '../assets/search.png';
 
-const Header = () => (
-  <header>
-    <div className="icon">
-      <img src={DiscLogo} alt="music-disc" />
-      <h3>Songify</h3>
-    </div>
+const Header = () => {
+  const [text] = useState(null);
 
-    <img src={SearchIcon} alt="search-icon" />
-  </header>
-);
+  return (
+    <header className="header">
+      <div className="icon">
+        <img src={DiscLogo} alt="music-disc" />
+        <h3>Songify</h3>
+      </div>
+
+      <input type="text" value={text} placeholder="Search" />
+
+      <img src={SearchIcon} alt="search-icon" />
+    </header>
+  );
+};
 
 export default Header;
